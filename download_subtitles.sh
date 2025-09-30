6eb8b9787ef26eca66377bc8016e91eec010b888
sizemo <aesizemo@gmail.com>
2025-09-29 23:17:52 -0500

    added line breaks and request sleep

--- download_subtitles.sh
+++ download_subtitles.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Set the channel URL
-CHANNEL_URL="https://www.youtube.com/@mpwdigital6243/"
+CHANNEL_URL="https://www.youtube.com/@mpwdigital6243/streams/"
 
 # Use yt-dlp with a filter for video titles
-yt-dlp -P '/Users/alexandra/Documents/Programming/mpwbot/subtitles' "$CHANNEL_URL" --skip-download --write-auto-subs --sub-format srt --match-filters "title ~= ^(BlueSky Live|Oxford Exxon Podcast)" "was_live = TRUE"
+yt-dlp -P '/Users/alexandra/Documents/Programming/mpwbot/subtitles' "$CHANNEL_URL" --skip-download\
+ --write-auto-subs --sub-format srt --match-filters "title ~= ^(BlueSky Live|Oxford Exxon Podcast)" "was_live = TRUE"\
+ --sleep-requests 35
