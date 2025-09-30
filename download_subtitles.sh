ad97e17855934d1f2600b6f9cb7b613188a7a1f6
sizemo <aesizemo@gmail.com>
2025-09-29 22:13:00 -0500

    initial upload of download_subtitles.sh

---
+++ download_subtitles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the channel URL
+CHANNEL_URL="https://www.youtube.com/@mpwdigital6243/"
+
+# Use yt-dlp with a filter for video titles
+yt-dlp -P '/Users/alexandra/Documents/Programming/mpwbot/subtitles' "$CHANNEL_URL" --skip-download --write-auto-subs --sub-format srt --match-filters "title ~= ^(BlueSky Live|Oxford Exxon Podcast)" "was_live = TRUE"
